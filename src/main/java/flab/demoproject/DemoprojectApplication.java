9fbeb2067755e3bbf8f1ea1a7cecccccb8641b5e
skskskdd22 <skskskdd22@naver.com>
2022-02-12 12:59:35 +0900

    push test

--- src/main/java/flab/demoproject/DemoprojectApplication.java
+++ src/main/java/flab/demoproject/DemoprojectApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoprojectApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoprojectApplication.class, args);
+
+        System.out.println("푸쉬!");
+    }
 
 }
