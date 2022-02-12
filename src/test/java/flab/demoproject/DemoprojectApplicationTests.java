ca1a0c8b5d375b50a376b59e4a839539f85d94f3
skskskdd22 <skskskdd22@naver.com>
2022-02-12 12:21:07 +0900

    Initial commit

---
+++ src/test/java/flab/demoproject/DemoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.demoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
